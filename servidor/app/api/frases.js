152a7fcf11c14b523a79f582baa582f707100e26
Pedro Raul <pedroraul101@gmail.com>
2018-10-19 16:55:39 -0300

    upgread

--- servidor/app/api/frases.js
+++ servidor/app/api/frases.js
@@ -1,16 +1,16 @@
 var api = {};
 
 var frases = [
-	{_id: 0, texto:'Alura, Cursos online de tecnologia que reinventam sua carreira.', tempo: 15 },
-	{_id: 1, texto:'Debuggers não consertam erros, apenas os exibem em slow motion.',tempo: 8 },
-	{_id: 2, texto:'Caelum, Ensino e Inovação.', tempo: 5 },
-	{_id: 3, texto:'Existem duas tarefas difíceis na Ciência da Computação: invalidação de cache e nomear as coisas.', tempo: 15 },
-	{_id: 4, texto:'Ciência da computação é tão sobre computadores quanto astronomia é sobre telescópios.', tempo: 15 },
-	{_id: 5, texto:'Na minha máquina funciona.', tempo: 5 },
-	{_id: 6, texto:'Hardware é o que você chuta, software é o que você xinga.', tempo: 12 },
-	{_id: 7, texto:'Software em funcionamento mais que documentação abrangente.', tempo: 10 },
-	{_id: 8, texto:'Iterar é humano, recursão é divino.', tempo: 7},
-	{_id: 9, texto:'Existem três jeitos de desenvolver software. O jeito certo, o jeito errado e o meu jeito, que é igual o jeito errado só que mais rápido.', tempo: 20},
+	{_id: 0, texto:'Current elections in Brazil have become a gigantic spectacle; the politicians are the ventriloquists and the people as puppets!', tempo: 20 },
+	{_id: 1, texto:'Debuggers do not fix bugs, they only display them in slow motion.',tempo: 8 },
+	{_id: 2, texto:'Life is what we do as we go through it.', tempo: 5 },
+	{_id: 3, texto:'There are two difficult tasks in Computer Science: cache overriding and naming things.', tempo: 15 },
+	{_id: 4, texto:'Computer science is as about computers as astronomy is about telescopes.', tempo: 15 },
+	{_id: 5, texto:'On my machine it works.', tempo: 5 },
+	{_id: 6, texto:'Hardware is what you kick, software is what you scold.', tempo: 12 },
+	{_id: 7, texto:'Software in operation rather than comprehensive documentation.', tempo: 10 },
+	{_id: 8, texto:'Iterar is human, recursion is divine.', tempo: 7},
+	{_id: 9, texto:'There are three ways to develop software. The right way, the wrong way, and my way, which is just the wrong way, but faster.', tempo: 20},
 
 	];
 
